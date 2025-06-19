dccbece57be0fbd3ee51ab2dba55243b9ef10db3
unknown <phungnhattan0305@gmail.com>
2025-06-19 18:48:27 +0700

    fix! register ui

---
+++ todoapp/app/clientcomponent/inputs/Input.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+import { BiDollar } from "react-icons/bi";
+
+interface InputProps {
+    id: string;
+    label: string;
+    type?: string;
+    required?: boolean;
+    disabled?: boolean;
+    placeholder?: string;
+    formatPrice?: boolean;
+    errors: FieldErrors;
+    register: UseFormRegister<FieldValues>;
+}
+
+const Input: React.FC<InputProps> = (
+    {
+        id,
+        label,
+        type = 'text',
+        required,
+        disabled,
+        placeholder,
+        formatPrice,
+        errors,
+        register
+    }
+) => {
+    return (
+        <div className="w-full relative">
+            {formatPrice && (
+                <BiDollar
+                    size={24}
+                    className="text-neutral-600 abosolute left-2 top-5" />
+            )}
+            <input
+                id={id}
+                disabled={disabled}
+                {...register(id, { required })}
+                placeholder=" "
+                type={type}
+                className={`peer w-full p-4 pt-6 font-light bg-white border-2 rounded-md outline-none disabled:opacity-0 disabled:cursor-not-allowed
+                    ${formatPrice ? 'pl-2' : 'pl-4'}
+                    ${errors[id] ? 'border-rose-500 focus:border-rose-500' : 'border-neutral-300 focus:border-black'}`} />
+            <label
+                className={`absolute text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0]
+                    peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0
+                    ${formatPrice ? 'left-9' : 'left-4'}
+                    ${errors[id] ? 'text-red-500' : 'text-zinc-400'}
+                    `}>
+                {label}
+            </label>
+        </div>
+    )
+}
+
+export default Input;
---
+++ todoapp/app/clientcomponent/modals/Modal.tsx
@@ -0,0 +1,109 @@
+'use client';
+
+import React, { useCallback, useEffect } from "react";
+import { IoMdClose } from "react-icons/io";
+import Button from "../Button";
+
+interface ModalProps {
+    isOpen?: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disabled?: boolean;
+    secondaryAction?: () => void;
+    secondaryActionLabel?: string;
+}
+
+const Modal: React.FC<ModalProps> = (
+    {
+        isOpen,
+        onClose,
+        onSubmit,
+        title,
+        body,
+        footer,
+        actionLabel,
+        disabled,
+        secondaryAction,
+        secondaryActionLabel
+    }
+) => {
+    const [showModal, setShowModal] = React.useState(isOpen);
+
+    useEffect(() => { setShowModal(isOpen) }, [isOpen]);
+
+    const handleClose = useCallback(() => {
+        if (disabled) return;
+        setShowModal(false);
+        setTimeout(() => { onClose(); }, 300);
+    }, [disabled, onClose]);
+
+    const handleSubmmit = useCallback(() => {
+        if (disabled) return;
+        onSubmit();
+    }, [disabled, onSubmit]);
+
+    const handleSecondaryAction = useCallback(() => {
+        if (disabled || !secondaryAction) return;
+        secondaryAction();
+    }, [disabled, secondaryAction]);
+
+    if (!isOpen) {
+        return null;
+    }
+
+    return (
+        <>
+            <div
+                className="flex justify-center items-center fixed inset-0 z-50 bg-neutral-800/70">
+                <div
+                    className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 2xl:w-1/4 h-full md:h-auto">
+                    {/* */}
+                    <div
+                        className="
+                            h-full
+                            ${showModal ? 'opacity-100' : 'opacity-0'}
+                            ${showModal ? 'translate-y-0' : 'translate-y-full'}">
+                        <div
+                            className="relative flex flex-col w-full translate h-full border-0 bg-white shadow-lg outline-none focus:outline-none">
+                            {/* Header */}
+                            <div
+                                className="flex items-center rounded-t-lg p-6 justify-center relative border-b-[1px]">
+                                <button
+                                    className="p-2 border-0 hover:opacity-70 transition absolute left-9">
+                                    <IoMdClose size={18} onClick={handleClose} />
+                                </button>
+                                <div
+                                    className="text-lg font-semibold">
+                                    {title}
+                                </div>
+                            </div>
+                            {/* Body */}
+                            <div
+                                className="relative p-6 flex-auto">
+                                {body}
+                            </div>
+                            {/* Footer */}
+                            <div
+                                className="flex flex-col gap-2 p-6">
+                                <div
+                                    className="flex flex-row gap-4 w-full items-center">
+                                    {secondaryAction && secondaryActionLabel && (
+                                        <Button outline onClick={handleSecondaryAction} type="button" label={secondaryActionLabel} disabled={disabled}/>
+                                    )}
+                                    <Button onClick={handleSubmmit} type="button" label={actionLabel} disabled={disabled} />
+                                </div>
+                                {footer}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Modal;
