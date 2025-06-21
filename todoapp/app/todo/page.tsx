2852186dcbfc782103c80de008cfb752a88a682a
unknown <phungnhattan0305@gmail.com>
2025-06-21 22:37:58 +0700

    fix! api route

--- todoapp/app/todo/page.tsx
+++ todoapp/app/todo/page.tsx
@@ -2,7 +2,10 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 import NoteList from "@/app/clientcomponent/todos/NoteList";
 
 export default async function Todo() {
-    const fetchData = await (await fetch("/api/todo")).json();
+    const baseUrl = process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : 'http://localhost:3000';
+    const fetchData = await (await fetch(`${baseUrl}/api/todo`)).json();
     console.log(fetchData);
     const currentUser = await getCurrentUser();
     return (
