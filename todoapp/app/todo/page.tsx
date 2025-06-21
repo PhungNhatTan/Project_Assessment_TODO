15dcbda85d8d4f6561ef858aeb334576b389cd33
unknown <phungnhattan0305@gmail.com>
2025-06-21 22:32:16 +0700

    fix! view todos

--- todoapp/app/todo/page.tsx
+++ todoapp/app/todo/page.tsx
@@ -2,8 +2,10 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 import NoteList from "@/app/clientcomponent/todos/NoteList";
 
 export default async function Todo() {
+    const fetchData = await (await fetch("/api/todo")).json();
+    console.log(fetchData);
     const currentUser = await getCurrentUser();
     return (
-        <NoteList currentUser={currentUser}/>
+        <NoteList currentUser={currentUser} />
     )
 }
